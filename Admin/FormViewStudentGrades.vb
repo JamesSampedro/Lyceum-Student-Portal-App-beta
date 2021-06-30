a918dffd79c9e54e7bdcfcf90e445d51f9d15189
James.Sampedro <72242960+Votum38git@users.noreply.github.com>
2021-06-30 22:22:30 +0800

    final

--- Admin/FormViewStudentGrades.vb
+++ Admin/FormViewStudentGrades.vb
@@ -13,18 +13,18 @@ Public Class FormViewStudentGrades
 
     End Sub
 
-    Private Sub DataGridView2_CellClick(sender As Object, e As DataGridViewCellEventArgs) Handles dgvStudentList.CellClick
+    Private Sub dgvStudentList_CellClick(sender As Object, e As DataGridViewCellEventArgs) Handles dgvStudentList.CellClick
         Dim conn As New OleDbConnection(cs)
         Dim i As Integer
         Dim io As String
-        io = CStr(i) 'conversion of Integer to String because Ref# or SL_ID/Loan Amount is an Integer value and won't be valid to call below without this
+        io = CStr(i)
 
         'Fetch the cell data to textbox value
         i = dgvStudentList.CurrentRow.Index
         Me.txtboxStudentID.Text = dgvStudentList.Item(0, i).Value
     End Sub
 
-    Private Sub btnView_Click(sender As Object, e As EventArgs) Handles btnView.Click
+    Private Sub btnView1stSem_Click(sender As Object, e As EventArgs) Handles btnView1stSem.Click
         Dim conn As New OleDbConnection(cs)
 
         Dim data1stsem As String = "SELECT * FROM tblGrades WHERE StudentID=@StudentID AND Semester='1st'"
@@ -36,6 +36,11 @@ Public Class FormViewStudentGrades
 
         dgv2ndSem.DataSource = table1stsem.DefaultView
 
+    End Sub
+
+    Private Sub btnView2ndSem_Click(sender As Object, e As EventArgs) Handles btnView2ndSem.Click
+        Dim conn As New OleDbConnection(cs)
+
         Dim data2ndsem As String = "SELECT * FROM tblGrades WHERE StudentID=@StudentID AND Semester='2nd'"
         Dim datafetch2ndsem As New OleDbCommand(data2ndsem, conn)
         datafetch2ndsem.Parameters.AddWithValue("@StudentID", txtboxStudentID.Text)
@@ -44,6 +49,10 @@ Public Class FormViewStudentGrades
         adapter2ndsem.Fill(table2ndsem)
 
         dgv2ndSem.DataSource = table2ndsem.DefaultView
+
     End Sub
 
+    Private Sub dgv1stSem_CellContentClick(sender As Object, e As DataGridViewCellEventArgs)
+
+    End Sub
 End Class
